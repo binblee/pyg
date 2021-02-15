cf262c85548e0c22a138460cbbf653919081b503
binblee <binblee@users.noreply.github.com>
2021-02-15 22:20:26 +0800

    food show up

--- snake/main.py
+++ snake/main.py
@@ -1,11 +1,12 @@
 import pygame
+import random
 from pygame.constants import (
     KEYDOWN,
     K_DOWN,
     K_ESCAPE,
     K_LEFT,
     K_RIGHT,
-    K_UP,
+    K_UP, K_g,
     QUIT)
 from pygame.sprite import Sprite
 
@@ -22,6 +23,9 @@ NORTH = (0, -1 * GRID_SIZE)
 SOUTH = (0, 1 * GRID_SIZE)
 WEST = (-1 * GRID_SIZE, 0)
 EAST = (1 * GRID_SIZE, 0)
+
+INITIAL_SNAKE_LENGTH = 3
+SNAKE_LENGTH_INC = 1
 
 
 class SnakeBody(Sprite):
@@ -42,6 +46,7 @@ class Snake():
             body = SnakeBody(i)
             self.body.append(body)
         self.direction = NORTH
+        self.last_tail_pos = self.body[-1].rect.topleft
 
     def update(self, pressed_keys):
         if pressed_keys[K_UP] and self.direction != SOUTH:
@@ -52,7 +57,7 @@ class Snake():
             self.direction = WEST
         if pressed_keys[K_RIGHT] and self.direction != WEST:
             self.direction = EAST
-
+        self.last_tail_pos = self.body[-1].rect.topleft
         next_pos = self.body[0].rect.topleft
         for i in range(len(self.body)-1):
             tmp = self.body[i+1].rect.topleft
@@ -68,35 +73,64 @@ class Snake():
         if self.body[0].rect.bottom > SCREEN_HEIGHT:
             self.body[0].rect.bottom = SCREEN_HEIGHT
 
+    def inc_length(self):
+        new_tail = SnakeBody(0)
+        self.body.append(new_tail)
+        new_tail.rect.topleft = self.last_tail_pos
 
-def draw_background(screen):
+    def get_positions(self):
+        positions = []
+        for bd in self.body:
+            positions.append(bd.rect.topleft)
+        return positions
+
+
+class Food(Sprite):
+    def __init__(self, avoid_positions):
+        super().__init__()
+        self.surf = pygame.Surface((GRID_SIZE, GRID_SIZE))
+        self.surf.fill((200, 0, 0))
+        pos = ()
+        good_position = False
+        while not good_position:
+            pos = (random.randint(0, POS_X_RANGE) * GRID_SIZE,
+                   random.randint(0, POS_Y_RANGE) * GRID_SIZE)
+            if pos not in avoid_positions:
+                good_position = True
+        self.rect = self.surf.get_rect(topleft=pos)
+
+
+def draw_background(screen, grid_on):
     COLOR_BACKGROUND = (0, 0, 0)
     COLOR_GRID = (200, 200, 200)
     screen.fill(COLOR_BACKGROUND)
-    for x in range(GRID_SIZE, SCREEN_HEIGHT, GRID_SIZE):
-        pygame.draw.line(
-            surface=screen,
-            color=COLOR_GRID,
-            start_pos=(0, x),
-            end_pos=(SCREEN_WIDTH, x),
-            width=1
-        )
-    for x in range(GRID_SIZE, SCREEN_WIDTH, GRID_SIZE):
-        pygame.draw.line(
-            surface=screen,
-            color=COLOR_GRID,
-            start_pos=(x, 0),
-            end_pos=(x, SCREEN_HEIGHT),
-            width=1
-        )
+    if grid_on:
+        for x in range(GRID_SIZE, SCREEN_HEIGHT, GRID_SIZE):
+            pygame.draw.line(
+                surface=screen,
+                color=COLOR_GRID,
+                start_pos=(0, x),
+                end_pos=(SCREEN_WIDTH, x),
+                width=1
+            )
+        for x in range(GRID_SIZE, SCREEN_WIDTH, GRID_SIZE):
+            pygame.draw.line(
+                surface=screen,
+                color=COLOR_GRID,
+                start_pos=(x, 0),
+                end_pos=(x, SCREEN_HEIGHT),
+                width=1
+            )
 
 
-snake = Snake(5)
-
+snake = Snake(INITIAL_SNAKE_LENGTH)
+avoid_positions = snake.get_positions()
+food = Food(avoid_positions)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 running = True
 clock = pygame.time.Clock()
+grid_on = False
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -106,13 +140,21 @@ while running:
                 running = False
     pressed_keys = pygame.key.get_pressed()
     snake.update(pressed_keys)
+    if pygame.sprite.collide_rect(food, snake.body[0]):
+        snake.inc_length()
+        food.kill()
+        avoid_positions = snake.get_positions()
+        food = Food(avoid_positions)
+    if pressed_keys[K_g]:
+        grid_on = not grid_on
 
-    draw_background(screen)
+    draw_background(screen, grid_on)
     for sprite in snake.body:
         screen.blit(sprite.surf, sprite.rect)
+    screen.blit(food.surf, food.rect)
 
     pygame.display.flip()
-    clock.tick(10)
+    clock.tick(8)
 
 
 pygame.quit()
