e75d9759795bff3cebdf45a3829e6ebbb076f476
binblee <binblee@users.noreply.github.com>
2021-08-11 21:05:32 +0800

    minimax algorithm for computer player

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,43 +1,46 @@
-from typing import Tuple
 import random
+import math
 
 
 class Board:
     EMPTY = '.'
 
-    def __init__(self) -> None:
+    def __init__(self):
         self.board = [self.EMPTY for _ in range(9)]
 
-    def print(self) -> None:
+    def print(self):
         for row in [self.board[i*3:i*3+3] for i in range(3)]:
             print(' | ' + ' | '.join(row) + ' | ')
         print('')
 
-    def position_avaliable(self) -> list:
-        return [pos for pos, mark in enumerate(self.board)
+    def available_moves(self) -> list:
+        return [square for square, mark in enumerate(self.board)
                 if mark == self.EMPTY]
 
-    def is_valid_move(self, pos) -> bool:
-        return pos in self.position_avaliable()
+    def is_valid_move(self, square) -> bool:
+        return square in self.available_moves()
 
-    def make_move(self, pos, mark) -> int:
-        valid_move = self.is_valid_move(pos)
+    def make_move(self, square, mark) -> bool:
+        valid_move = self.is_valid_move(square)
         if valid_move:
-            self.board[pos] = mark
+            self.board[square] = mark
         return valid_move
 
-    def is_win(self, pos) -> bool:
-        mark = self.board[pos]
+    def undo_move(self, square):
+        self.board[square] = self.EMPTY
+
+    def is_win(self, square) -> bool:
+        mark = self.board[square]
         # check row
-        row = pos // 3
+        row = square // 3
         if all([m == mark for m in self.board[row*3: row*3+3]]):
             return True
         # check col
-        col = pos % 3
+        col = square % 3
         if all([self.board[3*i+col] == mark for i in range(3)]):
             return True
-        # check diagonal, pos in [0, 2, 4, 6, 8]
-        if pos % 2 == 0:
+        # check diagonal, square in [0, 2, 4, 6, 8]
+        if square % 2 == 0:
             if all([self.board[i] == mark for i in (0, 4, 8)]):
                 return True
             if all([self.board[i] == mark for i in (2, 4, 6)]):
@@ -46,47 +49,105 @@ class Board:
 
 
 class Player:
-    def __init__(self, mark, board: Board) -> None:
+    def __init__(self, mark, board: Board):
         self.mark = mark
         self.board = board
 
     def move(self):
-        return random.choices(self.board.position_avaliable())[0]
+        return random.choices(self.board.available_moves())[0]
 
 
 class HumanPlayer(Player):
-    def __init__(self, mark, board: Board) -> None:
+    def __init__(self, mark, board: Board):
         super().__init__(mark, board)
 
-    def move(self) -> Tuple[int, str]:
-        index = None
-        valid_positions = self.board.position_avaliable()
-        while index not in valid_positions:
+    def move(self):
+        square = None
+        available_moves = self.board.available_moves()
+        while square not in available_moves:
             try:
-                index = int(input(f'Your turn {valid_positions}:'))
-                if index not in valid_positions:
+                square = int(input(f'Your turn {available_moves}:'))
+                if square not in available_moves:
                     raise ValueError
             except ValueError:
-                print(f'Invalid move {index}, try again.')
-        return index
+                print(f'Invalid move {square}, try again.')
+        return square
+
+
+class MinimaxPlayer(Player):
+    def __init__(self, mark, board: Board):
+        super().__init__(mark, board)
+
+    def move(self):
+        best_move = self.__minimax(self.mark)
+        return best_move['square']
+
+    def is_original(self, mark):
+        return self.mark == mark
+
+    def __minimax(self, player_mark):
+        best = {'square': None, 'score': None}
+        if self.is_original(player_mark):
+            best['score'] = -math.inf
+        else:
+            best['score'] = math.inf
+        available_moves = self.board.available_moves()
+        if len(available_moves) == 9:
+            best['square'] = random.choices(available_moves)[0]
+            return best
+        for move in available_moves:
+            ok = self.board.make_move(move, player_mark)
+            assert(ok)
+            score = self.__evaluate(move, player_mark)
+            if score is None:
+                opponent = self.__switch_player(player_mark)
+                square_score = self.__minimax(opponent)
+                score = square_score['score']
+            self.board.undo_move(move)
+            if self.is_original(player_mark):
+                if score > best['score']:
+                    best = {'square': move, 'score': score}
+            else:
+                if score < best['score']:
+                    best = {'square': move, 'score': score}
+        return best
+
+    def __switch_player(self, player_mark):
+        if player_mark == 'X':
+            return 'O'
+        else:
+            return 'X'
+
+    def __evaluate(self, move, mark):
+        num_available_move = len(self.board.available_moves())
+        score = None
+        if self.board.is_win(move):
+            if self.mark == mark:
+                score = 1 * (num_available_move + 1)
+            else:
+                score = - 1 * (num_available_move + 1)
+        elif num_available_move == 0:
+            score = 0
+        return score
 
 
 class Game:
-    def __init__(self) -> None:
+    def __init__(self):
         self.board = Board()
         self.player1 = HumanPlayer('X', self.board)
-        self.player2 = Player('O', self.board)
+        self.player2 = MinimaxPlayer('O', self.board)
 
     def play(self):
         player = self.player1
         self.board.print()
         winner = None
-        while self.board.position_avaliable() and not winner:
+        while self.board.available_moves() and not winner:
             print(f'{player.mark} move:')
-            index = player.move()
-            self.board.make_move(index, player.mark)
+            move = player.move()
+            ok = self.board.make_move(move, player.mark)
+            assert(ok)
             self.board.print()
-            if self.board.is_win(index):
+            if self.board.is_win(move):
                 winner = player
                 break
             if player == self.player1:
